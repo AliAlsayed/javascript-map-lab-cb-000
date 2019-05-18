e9875019ffe9de72636fc0da801f2835417dff23
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-18 17:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function(robot){
+  if knownDecepticons.includes(robot.name){
+    return Object.assign({}, robot, {
+      
+    })
+  }
+})
